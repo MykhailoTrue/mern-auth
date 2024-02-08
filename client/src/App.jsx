b34a563e657123b44bcd868f92e6213298ceb6d4
Mykhailo Trufyn <mishatrufyn@gmail.com>
2024-02-08 09:49:35 +0200

    install react.js and tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="text-red-500 flex-col-reverse flex">
+      <div>1s</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+    </div>
+  );
+}
+
+export default App;
